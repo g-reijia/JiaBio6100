19091cd9d3f78784fd82cbf4b558198dd457fb93
Rei <rei.jia@uvm.edu>
2025-04-30 13:24:43 -0400

    adding files

--- FileBatchr/Functions/FileBatcher.R
+++ FileBatchr/Functions/FileBatcher.R
@@ -8,10 +8,10 @@
 #         fun = name of operations functions
 # outputs: output_df with rows for each data file
 ########################################
-file_batcher <- function(file_names=NULL, fun=NULL,crunch_cols=NULL,param_names=NULL){
+file_batcher <- function(file_names=NULL, fun=NULL,param_names=NULL){
 
 # assign parameter defaults
-if (is.null(file_names)) return(print("no input"))
+if (is.null(file_names)) stop("no input")
 
 output_df <- as.data.frame(matrix(rep(NA,length(file_names)*length(param_names)),nrow=length(file_names),ncol=length(param_names)))
 
@@ -24,8 +24,7 @@ for (i in 1:length(file_names)){
                      header = TRUE,
                      sep=",")
     . <- fun(df=df,
-                     crunch_cols=unlist(crunch_cols),
-                     param_names = param_names)
+                param_names = param_names)
 
     output_df[i,] <- .
     nobs[i] <- nrow(df)
@@ -36,9 +35,7 @@ output_df
 output_df <- cbind(ID=1:length(file_names),file=basename(unlist(file_names)),nobs=nobs,output_df)
 }
 
-
 return(output_df)
-
-} # end of function file_batcher
+ # end of function file_batcher
 # --------------------------------------
 # file_batcher()
